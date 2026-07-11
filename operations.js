cc28b86b0e4a9e3ccfb7ab2e9055245d2edb8fb0
aryanmaurya-prog <mauryaaryan4910@gmail.com>
2026-07-12 02:14:50 +0530

    Operations In Javascript

---
+++ operations.js
@@ -0,0 +1,16 @@
+let value = 10
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(10+10);
+// console.log(10-10);
+// console.log(10*10);
+// console.log(10**10);
+// console.log(10/10);
+// console.log(10&10);
+
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
