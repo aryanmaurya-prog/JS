ff7fe297f649cc56f87ea1fe4644c671d8f98033
aryanmaurya-prog <mauryaaryan4910@gmail.com>
2026-07-12 01:45:13 +0530

    Conversion Of Data Types

---
+++ conversions.js
@@ -0,0 +1,28 @@
+let score = "33ab"
+
+console.log(typeof score) // string
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abd" => NaN
+// true => 1
+// false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let checking1 = ""
+let booleanIsLoggedIn1 = Boolean(checking1)
+console.log(booleanIsLoggedIn1)
+
+let checking2 = "aryan"
+let booleanIsLoggedIn2 = Boolean(checking2)
+console.log(booleanIsLoggedIn2)
+
+let checking3 = undefined
+let booleanIsLoggedIn3 = Boolean(checking3)
+console.log(booleanIsLoggedIn3)
