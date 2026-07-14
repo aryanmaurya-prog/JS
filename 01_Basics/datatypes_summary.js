3eb0d526da6745ed9d7f9856b2ec0efc043745fe
aryanmaurya-prog <mauryaaryan4910@gmail.com>
2026-07-15 01:40:42 +0530

    Organize JavaScript basics into 01_Basics

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// primitive 
+// 7 types  : string , number , boolean , null , undefined , symbol , bigint
+
+const score = 100
+const scoreValue = 100.9
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof(myFunction));
+
+const webseries_name = ["weak hero", "if wishes could kill", "asur", "vincenzo"]
+
+console.log(typeof(webseries_name));
